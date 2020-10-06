e85c7c850936f9a890738c0ef39b42472c71ad62
Sazedur Rahman <sazedursazu99@gmail.com>
2020-10-07 01:12:01 +0600

    updated

--- resources/views/frontend/partials/styles.blade.php
+++ resources/views/frontend/partials/styles.blade.php
@@ -2,15 +2,15 @@
 		<link rel="shortcut icon" type="image/x-icon" href="{{asset('frontend/assets/img/favicon.ico')}}">
 
 		<!-- CSS here -->
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.min.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/owl.carousel.min.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/slicknav.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/flaticon.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/animate.min.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/magnific-popup.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/fontawesome-all.min.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/themify-icons.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/slick.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/nice-select.css')}}">
-            <link rel="stylesheet" href="{{asset('owlcarousel/owl.theme.default.min.css')}}">
-            <link rel="stylesheet" href="{{asset('frontend/assets/css/style.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/bootstrap.min.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/owl.carousel.min.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/slicknav.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/flaticon.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/animate.min.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/magnific-popup.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/fontawesome-all.min.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/themify-icons.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/slick.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/nice-select.css')}}">
+            <link rel="stylesheet" href="{{asset('public/owlcarousel/owl.theme.default.min.css')}}">
+            <link rel="stylesheet" href="{{asset('public/frontend/assets/css/style.css')}}">
